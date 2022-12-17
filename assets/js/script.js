8c4c9628e80c214fd53ceb4ca78c2ab04d682dc9
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2022-12-16 21:30:34 -0700

    I have five days working

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ var fiveDay = document.getElementById("fiveDay");
 
 
 
-
+// Saving the users input city and then using fetch to get the daily weather
  function saveCityName() {
     inputCity = cityName.value;
     console.log(inputCity);
@@ -18,11 +18,11 @@ var fiveDay = document.getElementById("fiveDay");
 
 
 
-//  Fetching information from the openweather API, using a cities name
-function weatherFinder( city ) {
+//  Fetching weather information for the current day from the openweather API, using a cities name
+function weatherFinder(city) {
 
-    var key = '3c0dc64e8a4521b6469866fdc1fc689d';
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + key)  
+    var key = "3c0dc64e8a4521b6469866fdc1fc689d";
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key)  
     .then(function(response) { return response.json() })
     .then(function(data) {
       console.log(data);
@@ -33,7 +33,7 @@ function weatherFinder( city ) {
 
 
 //   Using the information gathered from the fetch function to display data attributes onto the web page for the user to see.
-  function weatherConditions( info ) {
+  function weatherConditions(info) {
 	// var celcius = Math.round(parseFloat(d.main.temp)-273.15);
 	var temp = Math.round(((parseFloat(info.main.temp)-273.15)*1.8)+32);
     var tempMin = Math.round(((parseFloat(info.main.temp_min)-273.15)*1.8)+32);
@@ -45,18 +45,51 @@ function weatherFinder( city ) {
     document.getElementById("description").innerHTML = (info.weather[0].description).charAt(0).toUpperCase() + (info.weather[0].description).slice(1);
 	document.getElementById("temp").innerHTML = temp + "&deg;";
     document.getElementById("tempRange").innerHTML = "Temperature Range: " + (tempMin + "&deg;") + " - " + (tempMax + '&deg;');
-    document.getElementById("coordLat").innerHTML = info.coord.lat;
-    document.getElementById("coordLon").innerHTML = info.coord.lon;
     document.getElementById("wind").innerHTML = info.wind.speed + " Wind Speed";
     document.getElementById("humidity").innerHTML = info.main.humidity + "% " + "Humidity";
     document.getElementById("feelsLike").innerHTML = "Feels like " + feelsLike + "&deg;";
+
+
+    var lat = info.coord.lat;
+    var lon = info.coord.lon;
+    fiveDayWeatherFinder(lat, lon);
 }
 
 
 
 
+// Fetching the 5 day forecast using lattitude and longitude.
+function fiveDayWeatherFinder(lat, lon) {
+
+  var key = "3c0dc64e8a4521b6469866fdc1fc689d";
+  fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + key)  
+  .then(function(response) { return response.json() })
+  .then(function(data) {
+    console.log(data);
+    fiveDayWeatherConditions(data);
+  });
+}
 
 
 
+function fiveDayWeatherConditions(info) {
+	// var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+
+for(var i=1; i<6; i++) {
+
+	var temp = Math.round(((parseFloat(info.list[i].main.temp)-273.15)*1.8)+32);
+    var tempMin = Math.round(((parseFloat(info.list[i].main.temp_min)-273.15)*1.8)+32);
+    var tempMax = Math.round(((parseFloat(info.list[i].main.temp_max)-273.15)*1.8)+32);
+    var feelsLike = Math.round(((parseFloat(info.list[i].main.feels_like)-273.15)*1.8)+32);
+    
+	
+	// document.getElementById("location1").innerHTML = info.list[1].name + ", " + info.list[1].sys.country;
+    document.getElementById("description" + i).innerHTML = (info.list[i].weather[0].description).charAt(0).toUpperCase() + (info.list[i].weather[0].description).slice(1);
+	document.getElementById("temp" + i).innerHTML = temp + "&deg;";
+    document.getElementById("tempRange" + i).innerHTML = "Temperature Range: " + (tempMin + "&deg;") + " - " + (tempMax + '&deg;');
+    document.getElementById("wind" + i).innerHTML = info.list[i].wind.speed + " Wind Speed";
+    document.getElementById("humidity" + i).innerHTML = info.list[i].main.humidity + "% " + "Humidity";
+    document.getElementById("feelsLike" + i).innerHTML = "Feels like " + feelsLike + "&deg;";
+}}
 
 
